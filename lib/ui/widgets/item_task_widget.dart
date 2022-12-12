cffc74fc5c375c17edf5f2dcd78847a0437d8453
Josh <201913147g@utea.edu.pe>
2022-12-12 01:55:38 -0500

    Implementando PopupMenuButton

--- lib/ui/widgets/item_task_widget.dart
+++ lib/ui/widgets/item_task_widget.dart
@@ -24,36 +24,81 @@ class ItemTaskWidget extends StatelessWidget {
           ),
         ],
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Stack(
         children: [
-          ItemCategoryWidget(
-            text: taskModel.category,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ItemCategoryWidget(
+                text: taskModel.category,
+              ),
+              divider3(),
+              Text(
+                taskModel.title,
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w600,
+                  color: kBrandPrimaryColor.withOpacity(0.85),
+                ),
+              ),
+              Text(
+                taskModel.description,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w500,
+                  color: kBrandPrimaryColor.withOpacity(0.75),
+                ),
+              ),
+              divider6(),
+              Text(
+                taskModel.date,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w500,
+                  color: kBrandPrimaryColor.withOpacity(0.75),
+                ),
+              ),
+            ],
           ),
-          divider3(),
-          Text(
-            taskModel.title,
-            style: TextStyle(
-              fontSize: 15.0,
-              fontWeight: FontWeight.w600,
-              color: kBrandPrimaryColor.withOpacity(0.85),
-            ),
-          ),
-          Text(
-            taskModel.description,
-            style: TextStyle(
-              fontSize: 14.0,
-              fontWeight: FontWeight.w500,
-              color: kBrandPrimaryColor.withOpacity(0.75),
-            ),
-          ),
-          divider6(),
-          Text(
-            taskModel.date,
-            style: TextStyle(
-              fontSize: 14.0,
-              fontWeight: FontWeight.w500,
-              color: kBrandPrimaryColor.withOpacity(0.75),
+          Positioned(
+            top: -10,
+            right: -12,
+            child: PopupMenuButton(
+              elevation: 2,
+              color: Colors.white,
+              icon: Icon(
+                Icons.more_vert,
+                color: kBrandPrimaryColor.withOpacity(0.85),
+              ),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14.0)),
+              onSelected: (value) {
+                print(value);
+              },
+              itemBuilder: (BuildContext context) {
+                return [
+                  PopupMenuItem(
+                    value: 1,
+                    child: Text(
+                      "Editar",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: kBrandPrimaryColor.withOpacity(0.85),
+                      ),
+                    ),
+                  ),
+                  PopupMenuItem(
+                    value: 2,
+                    child: Text(
+                      "Finalizar",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: kBrandPrimaryColor.withOpacity(0.85),
+                      ),
+                    ),
+                  ),
+                ];
+              },
             ),
           ),
         ],
