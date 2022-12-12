3af0a78e6061103b54d628fae7e6d645e355f2af
Josh <201913147g@utea.edu.pe>
2022-12-12 05:23:46 -0500

    Pagina de Login 1

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tasks/ui/general/colors.dart';
+import 'package:tasks/ui/widgets/button_normal_widget.dart';
+import 'package:tasks/ui/widgets/general.widgets.dart';
+import 'package:tasks/ui/widgets/textfield_normal_widget.dart';
+import 'package:tasks/ui/widgets/textfield_password_widget.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              divider30(),
+              SvgPicture.asset(
+                'assets/images/login.svg',
+                height: 180.0,
+              ),
+              divider30(),
+              Text(
+                "Iniciar Sesión",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600,
+                  color: kBrandPrimaryColor,
+                ),
+              ),
+              divider10(),
+              TextFieldNormalWidget(
+                hintText: "Correo Electronico",
+                icon: Icons.email,
+                controller: _emailController,
+              ),
+              divider10(),
+              divider6(),
+              TextFieldPasswordWidget(
+                controller: _passwordController,
+              ),
+              divider20(),
+              ButtonNormalWidget(onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
